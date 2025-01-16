1e6feee3f67e49c69a0b6d450d1cb4e5e775a05c
EyoelYTesfu <EyoelYTesfu@gmail.com>
2025-01-15 22:43:11 -0600

    Hello Climate

---
+++ backend/src/main/java/com/proclus/climasphere/ClimasphereApplication.java
@@ -0,0 +1,13 @@
+package com.proclus.climasphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClimasphereApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClimasphereApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/proclus/climasphere/ClimasphereApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proclus.climasphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClimasphereApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

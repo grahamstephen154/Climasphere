cd6158fb0a9700699511474771f1c2a8930c78e8
EyoelYTesfu <EyoelYTesfu@gmail.com>
2025-02-06 09:02:54 -0600

    Add background video integration with pexels.com
    Also:
    - Remove background images
    - Remove unecessary comments

--- frontend/public/index.js
+++ frontend/public/index.js
@@ -1,13 +1,14 @@
-import { env } from "./env.js";
+import { env } from './env.js';
 
 const host = env.host;
 const port = env.port;
+const pexelsAPI = env.pexelsAPI;
 
 document.getElementById('weatherForm').addEventListener('submit', async (event) => {
   event.preventDefault();
   const city = document.getElementById('city').value;
-  await fetchWeather(city);
-  changeBackgroundImage(city);
+  const queryToPexels = await fetchWeather(city);
+  await changeVideoSource(queryToPexels.weather[0].description);
 });
 
 async function fetchWeather(city) {
@@ -17,17 +18,17 @@ async function fetchWeather(city) {
       throw new Error(`Error: ${response.status}`);
     }
     const data = await response.json();
-    console.log(data);
     displayWeather(data);
+    return data;
   } catch (error) {
-    console.error(error);
     document.getElementById('weatherContainer').innerHTML = `<p>${error.message}</p>`;
+    return {error: 'error'};
   }
 }
 
 function displayWeather(data) {
   const weatherContainer = document.getElementById('weatherContainer');
-  weatherContainer.style.display = "block";
+  weatherContainer.style.display = 'block';
   weatherContainer.innerHTML = `
     <h2>Weather in ${data.name}</h2>
     <p>Temperature: ${data.main.temp}°C</p>
@@ -36,22 +37,23 @@ function displayWeather(data) {
   `;
 }
 
-function changeBackgroundImage(city) {
-  const body = document.body;
-  switch (city.toLowerCase()) {
-    case 'london':
-      body.style.backgroundImage = "url('images/london.jpg')";
-      break;
-    case 'wylie':
-      body.style.backgroundImage = "url('images/wylie.jpg')";
-      break;
-    case 'addis ababa':
-      body.style.backgroundImage = "url('images/addis_ababa.jpg')";
-      break;
-    default:
-      body.style.backgroundImage = "url('images/default.jpg')";
-      break;
-  }
-  body.style.backgroundSize = "cover";
-  body.style.backgroundPosition = "center";
+async function changeVideoSource(query) {
+  let backgroundVideoElement = document.getElementById('video-container');
+  let videoSource = document.getElementById('background-video-source');
+
+  let pexelResponse = await fetch(
+    `https://api.pexels.com/videos/search?query=${query}&per_page=1`, // Maybe take the last string of 'query' only
+    {
+      method: 'GET',
+      headers: {'Authorization': `${pexelsAPI}`}
+    });
+  let pexelResponseJson = await pexelResponse.json();
+  let pexelVideo = pexelResponseJson.videos[0].video_files[2].link; // TODO: The second array number (choices 0 to 6) signals the resolution of the video. Make it variable?
+                                                                    // TODO: The first array number signals a different picture everytime it's changed; the bigger the number, the less relevancy to the query
+
+  videoSource.setAttribute('src', `${pexelVideo}`);
+  videoSource.setAttribute('type', 'video/mp4');
+
+  backgroundVideoElement.load()
+  backgroundVideoElement.play()
 }
